6cd4159dc2dfeef079500990aceb8ae2bf7f8602
Emmanuel Joshua <40811019+EmmanuelJoshua@users.noreply.github.com>
2019-06-02 23:28:59 +0100

    Changes.

--- School-Management-System/src/school/management/system/SchoolManagementSystem.java
+++ School-Management-System/src/school/management/system/SchoolManagementSystem.java
@@ -19,7 +19,7 @@ public class SchoolManagementSystem extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/Login.fxml"));
         
         Scene scene = new Scene(root);
         
