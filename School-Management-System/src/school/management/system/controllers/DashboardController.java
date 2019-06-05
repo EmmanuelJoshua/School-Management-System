556b8d46da65c37a83f6810dc849a48dfd56a9a4
CodesManiac <joyajiboye2016@gmail.com>
2019-06-05 10:00:20 +0100

    Changes made to the Login file.

--- School-Management-System/src/school/management/system/controllers/DashboardController.java
+++ School-Management-System/src/school/management/system/controllers/DashboardController.java
@@ -3,25 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package school.management.system;
+package school.management.system.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  *
  * @author DELL
  */
 public class DashboardController implements Initializable {
+
+    @FXML
+    private AnchorPane dashboardPane;
     
  
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    // @FXML
+    // private void logoutBtn(ActionEvent event) throws IOException {
+    //     Stage stage = (Stage) dashboardPane.getScene().getWindow();
+    //     stage.close();
+    //     Parent root = FXMLLoader.load(getClass().getResource("/school/management/system/fxml/Login.fxml"));
+    //     Scene scene = new Scene(root);
+    //     stage.setScene(scene);
+    //     stage.show();
+    // }
     
 }
--- School-Management-System/src/school/management/system/controllers/LoginController.java
+++ School-Management-System/src/school/management/system/controllers/LoginController.java
@@ -5,31 +5,55 @@
  */
 package school.management.system.controllers;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  *
  * @author DELL
  */
 public class LoginController implements Initializable {
-    
     @FXML
-    private Label label;
-    
+    private JFXTextField userName;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private JFXPasswordField password;
+    @FXML
+    private AnchorPane loginPane;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void signIn(ActionEvent event) throws IOException {
+        String username = "Admin01";
+        String userPassword = "AdminAdmin";
+        if (userName.getText().equals(username) && password.getText().equals(userPassword)) {
+            Stage stage = (Stage) loginPane.getScene().getWindow();
+            stage.close();
+            Parent root = FXMLLoader.load(getClass().getResource("/school/management/system/fxml/Dashboard.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.NONE, "Incorrect Username or Password", ButtonType.OK);
+            alert.setTitle("Invalid");
+            alert.showAndWait();
+        }
+    }
+
 }
